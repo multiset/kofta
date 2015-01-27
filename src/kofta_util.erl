26bf7129d1f7539495d22e129c05f599981415a4
Benjamin Anderson <b@banjiewen.net>
2015-01-26 19:36:51 -0800

    Add metrics for parsed error codes

--- src/kofta_util.erl
+++ src/kofta_util.erl
@@ -2,14 +2,17 @@
 
 -export([error_to_atom/1]).
 
+-include("kofta.hrl").
+
 
 -spec error_to_atom(ErrorID) -> ErrorName when
     ErrorID :: integer(),
     ErrorName :: atom().
 
+error_to_atom(0) ->
+    ok;
 error_to_atom(ErrorID) ->
-    case ErrorID of
-        0 -> ok;
+    Message = case ErrorID of
         -1 -> unexpected_server_error;
         1 -> offset_out_of_range;
         2 -> invalid_message;
@@ -25,5 +28,10 @@ error_to_atom(ErrorID) ->
         12 -> offset_metadata_too_large;
         14 -> offsets_load_in_progress;
         15 -> consumer_coordinator_not_available;
-        16 -> not_coordinator_for_consumer
-    end.
+        16 -> not_coordinator_for_consumer;
+        _ ->
+            lager:warning("Got unknown ErrorID: ~p", [ErrorID]),
+            unknown
+    end,
+    ?INCREMENT_COUNTER([kofta, errors, Message]),
+    Message.
